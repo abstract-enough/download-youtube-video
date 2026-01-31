e537e60ece666e60f1b6c0593059ec2fde0d2ed2
abstract-enough <77987885+abstract-enough@users.noreply.github.com>
2026-01-31 15:28:07 -0500

    Update download_youtube_video.py

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,1 +1,12 @@
+from pytubefix import YouTube
 
+#replace with your video address
+video_link = "https://youtu.be/NMQVcvrKjfI"
+
+#replace witu folder location where you want to save the video
+save_folder = "/content/drive/MyDrive"
+
+
+yt1 = YouTube(video_link)
+video_stream = yt1.streams.get_highest_resolution()
+video_stream.download(save_folder)
